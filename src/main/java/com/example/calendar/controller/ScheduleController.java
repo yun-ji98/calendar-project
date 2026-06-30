255c7f7ca1e380d9cfdf2e7fd91c4bdadca229d3
yun-ji98 <lyj77731@naver.com>
2026-06-30 16:48:47 +0900

    service, controller 기본 틀

---
+++ src/main/java/com/example/calendar/controller/ScheduleController.java
@@ -0,0 +1,12 @@
+package com.example.calendar.controller;
+
+import com.example.calendar.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+}
--- src/main/java/com/example/calendar/service/ScheduleService.java
+++ src/main/java/com/example/calendar/service/ScheduleService.java
@@ -1,4 +1,13 @@
 package com.example.calendar.service;
 
+import com.example.calendar.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
 public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
 }
